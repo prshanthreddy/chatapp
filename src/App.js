5dbc9881ada07dcda3714be76821294339ba7455
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-12-28 12:16:18 -0600

    Added authContext

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Home from './pages/home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
+import { useContext } from 'react';
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
     <div className="App">
       <BrowserRouter>
