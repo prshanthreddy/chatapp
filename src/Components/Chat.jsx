6fecf6e3444d4b79f136952af7f9a3884080c430
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-12-29 18:30:26 -0600

    Minor changes

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,28 +1,3 @@
-// import React, { useContext } from "react";
-// import Messages from "./Messages";
-// import Input from "./Input";
-// import { ChatContext } from "../context/ChatContext";
-
-// const Chat = () => {
-//   const { data } = useContext(ChatContext);
-
-//   return (
-//     <div className="chat">
-//       <div className="chatInfo">
-//         <span>{data.user?.displayName}</span>
-//         <div className="chatIcons">
-//           <img src="https://img.icons8.com/material-rounded/24/000000/phone.png" alt="" />
-//           <img src="https://img.icons8.com/material-rounded/24/000000/video-call.png" alt="" />
-//           <img src="https://img.icons8.com/material-rounded/24/000000/info.png" alt="" />
-//         </div>
-//       </div>
-//         <Messages />
-//         <Input />
-//     </div>
-//   )
-// }
-
-// export default Chat
 import React, { useContext } from "react";
 import Messages from "./Messages";
 import Input from "./input";
