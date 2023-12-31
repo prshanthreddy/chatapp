1a978ae1e4e7184f69e3d6d2c2f971cf9c07644b
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-12-31 02:59:37 -0600

    No empty message is send on entering send Button

--- src/Components/input.jsx
+++ src/Components/input.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useContext } from 'react';
-import { ChatContext } from '../context/ChatContext';
-import { AuthContext } from '../context/AuthContext';
-import { Timestamp, arrayUnion, updateDoc } from 'firebase/firestore';
-import { v4 as uuid } from 'uuid';
-import { doc } from 'firebase/firestore';
-import { db, storage } from '../firebase';
-import { getDownloadURL, ref, uploadBytesResumable, } from 'firebase/storage';
-import { serverTimestamp } from 'firebase/firestore';
-import send from '../images/send-message.png';
+import React, { useState, useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { AuthContext } from "../context/AuthContext";
+import { Timestamp, arrayUnion, updateDoc } from "firebase/firestore";
+import { v4 as uuid } from "uuid";
+import { doc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { serverTimestamp } from "firebase/firestore";
+import send from "../images/send-message.png";
 
 const Input = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [file, setFile] = useState(null);
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
@@ -27,7 +27,7 @@ const Input = () => {
       message.file = downloadURL;
     }
 
-    await updateDoc(doc(db, 'chats', data.chatId), {
+    await updateDoc(doc(db, "chats", data.chatId), {
       messages: arrayUnion(message),
     });
   };
@@ -39,12 +39,12 @@ const Input = () => {
         const uploadTask = uploadBytesResumable(storageRef, file);
 
         uploadTask.on(
-          'state_changed',
+          "state_changed",
           (snapshot) => {
             // Handle progress if needed
           },
           (error) => {
-            console.error('Error uploading file:', error);
+            console.error("Error uploading file:", error);
             // Handle error
           },
           async () => {
@@ -52,7 +52,7 @@ const Input = () => {
               const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
               await updateChatWithMessage(downloadURL);
             } catch (error) {
-              console.error('Error getting download URL:', error);
+              console.error("Error getting download URL:", error);
               // Handle error
             }
           }
@@ -62,63 +62,66 @@ const Input = () => {
       }
 
       // Update user chats with last message and date
-      await updateDoc(doc(db, 'userChats', currentUser.uid), {
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
         [`${data.chatId}.lastMessage`]: {
           text,
         },
         [`${data.chatId}.date`]: serverTimestamp(),
       });
 
-      await updateDoc(doc(db, 'userChats', data.user.uid), {
+      await updateDoc(doc(db, "userChats", data.user.uid), {
         [`${data.chatId}.lastMessage`]: {
           text,
         },
         [`${data.chatId}.date`]: serverTimestamp(),
       });
 
-      setText('');
+      setText("");
       setFile(null);
     } catch (error) {
-      console.error('Error handling send:', error);
+      console.error("Error handling send:", error);
       // Handle error
     }
   };
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
+    if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault(); // Prevents adding a new line
       handleSend(); // Calls handleSend function when Enter is pressed
     }
   };
-
+  const isSendDisabled = () => {
+    return text === "" && !file;
+  };
   return (
-    <div className='input'>
+    <div className="input">
       <input
-        type='text'
-        placeholder='Type a message'
+        type="text"
+        placeholder="Type a message"
         value={text}
         onChange={(e) => setText(e.target.value)}
         onKeyPress={handleKeyPress}
       />
-      <div className='send'>
+      <div className="send">
         <input
-          type='file'
-          id='file'
-          style={{ display: 'none' }}
+          type="file"
+          id="file"
+          style={{ display: "none" }}
           onChange={(e) => setFile(e.target.files[0])}
         />
-        <label htmlFor='file'>
+        <label htmlFor="file">
           <img
-            src='https://img.icons8.com/material-rounded/24/000000/attach.png'
-            alt='Attach File'
+            src="https://img.icons8.com/material-rounded/24/000000/attach.png"
+            alt="Attach File"
           />
-        </label> 
-      <button onClick={handleSend} id='button' style={{ display: 'none' }}>
-        </button>        
-        <label htmlFor='button'>
-          <img
-            src={send}
-            alt='Send'
-          />
+        </label>
+        <button
+          onClick={handleSend}
+          disabled={isSendDisabled()}
+          id="button"
+          style={{ display: "none" }}
+        ></button>
+        <label htmlFor="button">
+          <img src={send} alt="Send" />
         </label>
       </div>
     </div>
