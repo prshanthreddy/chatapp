a33684b38e4da929baf809bc82cd944e0cba5615
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2023-12-30 23:38:42 -0600

    Revert "Add"
    This reverts commit 40f3299ffc413a70259280024e4d0b0fa35abbc6.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Chat App</span>
+        <span className="logo">Lama Chat</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
